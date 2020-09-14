12c6441c66c3fb54fc4ff73aeadab7379937771a
unknown <siddh.gupta99@gmail.com>
2020-09-14 13:30:27 +0530

    Bug fix

--- src/main/java/com/minorproject/homegarden/plants/PlantService.java
+++ src/main/java/com/minorproject/homegarden/plants/PlantService.java
@@ -7,12 +7,18 @@ import org.springframework.stereotype.Service;
 public class PlantService {
 	@Autowired
 	private PlantDetailRepository plantsRepo;
-	
+	@Autowired
+	private PlantCareDetailsRepository careRepo;
+
 	public PlantDetails addPlant(PlantDetails details) {
 		PlantDetails plant = new PlantDetails(details.getName(), details.getDescription());
 		plant.setCareInfo(details.getCareInfo());
 		plant.setTemperature(details.getTemperature());
 		plant.setWater(details.getWater());
+		PlantCareDetails care = new PlantCareDetails(details.getCareInfo().getWaterDetails(),
+				details.getCareInfo().getTemperatureDetails(), details.getCareInfo().getLightDetails(),
+				details.getCareInfo().getSoilDetails());
+		care.setPlantInfo(plant);
 		return plantsRepo.save(plant);
 	}
 }
